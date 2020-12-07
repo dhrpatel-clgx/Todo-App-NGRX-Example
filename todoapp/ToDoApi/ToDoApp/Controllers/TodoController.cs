cfcf64189169bb6da2afdc7bf65092f4928bf507
Dhruvin Patel <dhrpatel@corelogic.com>
2020-12-07 16:27:17 -0600

    Added missing files

---
+++ todoapp/ToDoApi/ToDoApp/Controllers/TodoController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoApp.Models;
+
+namespace ToDoApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private static List<Todo> _toDos = new List<Todo>();
+
+        public TodoController()
+        {
+        }
+
+        [HttpGet]
+        public ActionResult GetToDos()
+        {
+            return Ok(_toDos);
+        }
+
+        [HttpPost]
+        public ActionResult AddToDo(Todo newToDo)
+        {
+            _toDos.Add(newToDo);
+            return Ok(_toDos);
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteToDo([FromQuery]int index)
+        {
+            _toDos.RemoveAt(index);
+            return Ok(_toDos);
+        }
+    }
+}
---
+++ todoapp/todoapp/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ToDoComponent } from './to-do/component/to-do.component';
+import { ActionReducer, MetaReducer, StoreModule } from '@ngrx/store';
+import { reducer, ToDoReducer } from './to-do/todo.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { ToDoEffects } from './to-do/todo.effects';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+// console.log all actions
+export function debug(reducer: ActionReducer<any>): ActionReducer<any> {
+  return function(state, action) {
+    console.log('state', state);
+    console.log('action', action);
+ 
+    return reducer(state, action);
+  };
+}
+ 
+export const metaReducers: MetaReducer<any>[] = [debug];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToDoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    StoreModule.forRoot({ todos: ToDoReducer }, { metaReducers }),
+    EffectsModule.forRoot([ToDoEffects])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
