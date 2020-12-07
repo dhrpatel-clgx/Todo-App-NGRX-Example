df7f426988dd212f194115da27de5ac93624f4d5
Dhruvin Patel <dhrpatel@corelogic.com>
2020-12-07 16:36:25 -0600

    oops copied the wrong files

---
+++ todoapp/src/app/to-do/todo.reducer.ts
@@ -0,0 +1,31 @@
+import { Action, createReducer, on } from '@ngrx/store';
+import * as ToDoActions from './todo.action';
+import ToDo from './todo.model';
+import ToDoState, { initializeState } from './todo.state';
+
+export const intialState = initializeState();
+
+export const reducer = createReducer(
+  intialState,
+  on(ToDoActions.GetToDoAction, state => state),
+  on(ToDoActions.CreateToDoAction, (state: ToDoState, todo: ToDo) => {
+    return { ...state, ToDos: [...state.ToDos, todo], ToDoError: null };
+  }),
+  on(ToDoActions.SuccessGetToDoAction, (state: ToDoState, { payload }) => {
+    return { ...state, ToDos: payload };
+  }),
+  on(ToDoActions.SuccessCreateToDoAction, (state: ToDoState, { payload }) => {
+    return { ...state, ToDos: payload, ToDoError: null };
+  }),
+  on(ToDoActions.ErrorToDoAction, (state: ToDoState, error: Error) => {
+    console.log(error);
+    return { ...state, ToDoError: error };
+  }),
+  on(ToDoActions.SuccessDeleteToDoAction, (state: ToDoState, { payload }) =>  {
+    return { ...state, ToDos: payload, ToDoError: null };
+  })
+);
+
+export function ToDoReducer(state: ToDoState | undefined, action: Action) {
+  return reducer(state, action);
+}
